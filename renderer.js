e73e611fb579f569d79e76ba7f7f15cd507f97c3
sahasralaggani123-bit <sahasralaggani123@gmail.com>
2026-05-08 16:52:24 +0530

    Add files via upload

---
+++ renderer.js
@@ -0,0 +1,9 @@
+
+let previousVDOM = "";
+ export function render(vdom, container) {
+     if (vdom !== previousVDOM) {
+         container.innerHTML = vdom; 
+         previousVDOM = vdom; console.log("UI Updated"); 
+        } else { console.log("No Change"); 
+    
+        } }
